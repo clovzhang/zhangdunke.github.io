f08d65c9eb3412a4f6b64d4493e2b11290e67279
zhangdunke <dunkezhang@clubfactory.com>
2020-04-30 14:59:56 +0800

    feat(init): project

---
+++ precache-manifest.908e605ca7cd50bbffa0cf35c17d4619.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b5b758080c83887377f972fb574f59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03d9dfe16d3c4d1ee922",
+    "url": "/static/css/2.91bd1bea.chunk.css"
+  },
+  {
+    "revision": "4b1bdf81d4006f964930",
+    "url": "/static/css/main.518a4bc7.chunk.css"
+  },
+  {
+    "revision": "03d9dfe16d3c4d1ee922",
+    "url": "/static/js/2.b3ea671c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.b3ea671c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b1bdf81d4006f964930",
+    "url": "/static/js/main.7afd152c.chunk.js"
+  },
+  {
+    "revision": "455fb10cab36a18891be",
+    "url": "/static/js/runtime-main.96111ebb.js"
+  },
+  {
+    "revision": "be41cdb092b3035c0416f3c3674c6a6a",
+    "url": "/static/media/avatar.be41cdb0.jpeg"
+  },
+  {
+    "revision": "9ca78710b8d25b7ad5b7f9ed7c05b848",
+    "url": "/static/media/home-background.9ca78710.png"
+  },
+  {
+    "revision": "0177418d8badb9dd65d42c701296f978",
+    "url": "/static/media/index.0177418d.md"
+  },
+  {
+    "revision": "0bb4c781021fe6b0d7cb2d05753ee1e3",
+    "url": "/static/media/index.0bb4c781.md"
+  },
+  {
+    "revision": "65d1b90c6d46952c72e0d519347523ca",
+    "url": "/static/media/index.65d1b90c.md"
+  }
+]);
